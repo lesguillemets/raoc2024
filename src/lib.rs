7b24f43e800557549fea054e8633742466a501e9
lesguillemets <kgkacko+github@gmail.com>
2025-01-12 01:05:21 +0900

    idefult to report time

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::io::BufReader;
 use std::time::Instant;
 
 // [f0, f1] それぞれ &str -> () を受け取って日に応じて実行する
-pub fn run(parts: &[fn(&str); 2]) {
+pub fn run_silently(parts: &[fn(&str); 2]) {
     let args: Vec<String> = env::args().collect();
     let exec_file: &str = &args[0];
     let day = &exec_file[exec_file.len() - 2..exec_file.len()];
@@ -32,8 +32,13 @@ pub fn run(parts: &[fn(&str); 2]) {
     parts[part](&input);
 }
 
+// デフォルトで時間も表示するようにしましょう
+pub fn run(parts: &[fn(&str); 2]) {
+    run_and_report_time(parts);
+}
+
 pub fn run_and_report_time(parts: &[fn(&str); 2]) {
     let start = Instant::now();
-    run(parts);
+    run_silently(parts);
     println!("Elapsed time: {:?}", start.elapsed());
 }
