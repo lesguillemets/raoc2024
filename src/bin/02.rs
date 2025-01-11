bbd28b68ff83ec824e9664f403ea61e542a31b8b
lesguillemets <kgkacko+github@gmail.com>
2025-01-11 17:46:07 +0900

    0201

---
+++ src/bin/02.rs
@@ -0,0 +1,37 @@
+use raoc2024::run;
+use std::cmp::Ordering;
+
+struct Report {
+    dat: Vec<u32>,
+}
+
+impl Report {
+    fn from_line(l: &str) -> Self {
+        Report {
+            dat: l.split_whitespace().map(|n| n.parse().unwrap()).collect(),
+        }
+    }
+    fn is_safe(&self) -> bool {
+        let dir = self.dat[0].cmp(&self.dat[1]);
+        for (&xi, &xj) in self.dat.iter().zip(self.dat.iter().skip(1)) {
+            let diff = xi.abs_diff(xj);
+            if xi.cmp(&xj) != dir || diff > 3 || diff == 0 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+fn part1(s: &str) {
+    let reports: Vec<Report> = s.lines().map(Report::from_line).collect();
+    println!("{}", reports.iter().filter(|r| r.is_safe()).count());
+}
+
+fn part2(s: &str) {
+    println!("part 2");
+}
+
+fn main() {
+    run(&[part1, part2]);
+}
